87d7335c4710b3ee4b2db431a5e1e32be782125c
avulasailatha95-dot <avulasailatha95@gmail.com>
2026-04-14 15:08:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from Azure 🚀"
+    return "Hello Docker!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
